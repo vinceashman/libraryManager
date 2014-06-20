1774ceb3c52bc6e1eec1b1ac323e3b48e8c83d75
HatfieldGreg <hatfieldgreg@hotmail.com>
2014-06-19 18:43:21 -0600

    user sign in prompt before menu call

--- UserInterface.h
+++ UserInterface.h
@@ -1,9 +1,9 @@
 #ifndef UserInterface_H
 #define UserInterface_H
-#include <string>
-#include <vector>
-#include <cstdlib>
 
+//#include "book.h"
+#include <vector>
+#include <string>
 using namespace std;
 
 class UserInterface
@@ -23,7 +23,10 @@ class UserInterface
              //go through the user's book list and print the list of books with
              //a choice for the user to select which book/media to check in.
              //updates the book list to include the book to show that its available.
-             
+             std::cout <<"\nList of Available Books here in Valhalla:" << endl;
+             std::cout <<"|------------------------------------------|" << endl;
+             std::cout <<"|                                          |" << endl;
+             std::cout <<"|------------------------------------------|" << endl;
              DisplayMenu();
          }
          //user checks out book checks user number of books checked out
@@ -40,6 +43,11 @@ class UserInterface
             std::cout << "\nCheckOut is called" << endl;
             //go through book list and display which books are available to check out
             //user gives choice to what book they want to use.
+            //display in groups of 10
+            std::cout <<"\nList of Available Books here in Valhalla:" << endl;
+            std::cout <<"|------------------------------------------|" << endl;
+            std::cout <<"|                                          |" << endl;
+            std::cout <<"|------------------------------------------|" << endl;
             DisplayMenu();
          }
          void AdvanceDate()
@@ -47,22 +55,30 @@ class UserInterface
               std::cout << "\nAdvanceDate is called" << endl;
               DisplayMenu();
          }
+         //login menu call?
+        void DisplayLoginMenu()
+        {
+        std::cout << "User Login:" << endl;
+        string userName = promptForInput();
+        std::cout << "Welcome to the Library " << userName << endl;
+        //pass information to list?
+        DisplayMenu();
+        }
          //main menu call
         void DisplayMenu()
          {
             //add additional menu options when needed, update if statements to include new choices
-            std::cout << "\n----Library System----" << endl;
+            std::cout << "\n|---Library System---|" << endl;
             std::cout << "\\*------------------*/" << endl;
-            std::cout << "To Check In enter 1"<< endl;
+            std::cout << "\nTo Check In enter 1"<< endl;
             std::cout << "To Check Out enter 2" << endl;
             std::cout << "To Advance Date enter 3" << endl;
             std::cout << "To Exit enter *" << endl;
-            std::cout << "\\*------------------*/" << endl;
+            std::cout << "\n\\*------------------*/" << endl;
             //user choice for menu
             //input a simple string (can't use switch set up with a string, using if statements instead)
             //the correct choice will go onto the next function
-            //will inform user if the choice is invalid.
-            std::cout << "calling for input" << endl;
+            //will inform user if the choice is invalid.            
             string choice = promptForInput();
             
             std::cout << "\n";
@@ -90,7 +106,11 @@ class UserInterface
            }
 
          }//end of displaymenu function
+       
     private:
+            //adding a username variable for now to pass to other objects..
+            //unless main handles this..
+            string userName;
 };
 
 #endif // UI_H
